5109847bd0915c2bd674e57c9847f636927f6ba3
Rafael Alves <rafacorreaalves@hotmail.com>
2022-05-04 15:27:06 -0300

    teste 2

--- app/src/main/java/com/example/noteapp/presenter/activity/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/presenter/activity/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         configClickFab()
-        configClickMenu()
+        configClickMenu()dawda
     }
 
     private fun configView() {
